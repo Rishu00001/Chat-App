b991b89bd0b439a61b51b64d3687bbcd9ec4b629
Ritik Singh <121800617+Rishu00001@users.noreply.github.com>
2025-06-07 18:05:37 +0530

    Update auth.controllers.js

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -39,8 +39,8 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
     return res.status(201).json({
       message: "User successfully created",
@@ -72,8 +72,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
     return res.status(201).json({
       message: "User successfully logged in",
