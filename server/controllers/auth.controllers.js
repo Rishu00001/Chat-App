d754ce8efcc2e1e774adba5b97a3773551e9a013
Rishu00001 <ritiksingh7369@gmail.com>
2025-06-19 11:07:00 +0530

    deploy config

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -39,8 +39,8 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
     return res.status(201).json({
       message: "User successfully created",
@@ -72,8 +72,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
     return res.status(201).json({
       message: "User successfully logged in",
@@ -88,8 +88,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
   httpOnly: true,
-  secure: false,
-  sameSite: "Strict",
+  secure: true,
+  sameSite: "None",
 });
 
     return res.status(200).json({ message: "Successfully Logged Out" });
--- server/socket/socket.js
+++ server/socket/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: `http://localhost:5173`, // Update if frontend URL changes
+    origin: `https://wechat-22ij.onrender.com`, // Update if frontend URL changes
   },
 });
 
